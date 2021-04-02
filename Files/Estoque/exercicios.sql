9c1430896f0cc295b0c7dffa5a3e487095157682
NandoFilter <fernandohfilter10@gmail.com>
2021-04-02 18:36:02 -0300

    Update exercicios.sql

--- Files/Estoque/exercicios.sql
+++ Files/Estoque/exercicios.sql
@@ -9,10 +9,11 @@ produto está (caso ele esteja em prateleira) ou o número das seções (caso de
 produtos que não estão em prateleiras). É necessário garantir que todos os
 produtos apareçam, mesmo que não estejam em prateleiras ou seções. */
 
-SELECT p.codprod as cod_produto, p.descricao, pr.numprat as num_prateleira, pr.numsecao as num_secao
+SELECT p.codprod as produto, p.descricao as nome, pr.numprat as prateleira, s.numsecao as seção
 FROM produto p
-         LEFT JOIN prateleira pr ON p.codprod = pr.codprod
-         LEFT JOIN secao s ON pr.numsecao = s.numsecao
+         FULL OUTER JOIN prateleira pr ON p.codprod = pr.codprod
+         FULL OUTER JOIN secao s on pr.numsecao = s.numsecao;
+
 
 -- Exercício 02
 
@@ -30,5 +31,44 @@ FROM produto p,
 WHERE mv.codprod = p.codprod
   AND p.codgru = gp.codgru
   AND mv.datahora BETWEEN '01/06/2018' AND '30/06/2018'
-ORDER BY p.descricao
+ORDER BY p.descricao;
 
+-- Exercício 03
+
+/* O gerente de vendas da empresa precisa saber quais vendas foram feitas no mês
+de maio de 2018 e para quem estas foram feitas. Ele solicitou uma listagem das
+vendas com os seguintes dados: número da nota fiscal, data da emissão, nome do
+cliente, nome da cidade do cliente, além dos produtos de cada venda, mostrando
+código do produto, quantidade vendida e o valor total de cada produto vendido
+(quantidade * preço). */
+
+SELECT nfv.numnf                as num_notaFiscal,
+       nfv.dtemissao            as data_emissao,
+       c.fantasia,
+       cid.nome                 as nome_cidade,
+       pnfv.codprod             as cod_prod,
+       pnfv.qtde                as qntd_prod,
+       (pnfv.qtde * pnfv.preco) as preco
+FROM nfvenda nfv,
+     cliente c,
+     cidade cid,
+     prodnfvenda pnfv
+WHERE pnfv.numnf = nfv.numnf
+  AND nfv.codcli = c.codcli
+  AND c.codcid = cid.codcid
+  AND nfv.dtemissao BETWEEN '01/05/2018' AND '31/05/2018'
+
+-- Exercício 04
+
+/* Elabore uma consulta que mostre todas as movimentações feitas pelos
+funcionários 74 e 75 (considerar funcionário responsável pela movimentação).
+Mostrar o código e a descrição do produto, a data e hora da movimentação, o
+nome do funcionário responsável pela movimentação e a quantidade
+movimentada. */
+
+SELECT p.codprod, p.descricao as descricao_prod, mv.datahora, f.nome as nome_funcionario, mv.qtde
+FROM produto p,
+     movest mv,
+     funcionario f
+WHERE mv.codprod = p.codprod
+AND f.matfunc BETWEEN '74' AND '75';
