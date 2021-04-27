f56d68a80ea5cb9d36fb59d3652ed7dbbfeeea14
NandoFilter <fernandohfilter10@gmail.com>
2021-04-26 21:15:17 -0300

    04/26/21 - Edit Files

--- Files/Biblioteca/exercicio_06.sql
+++ Files/Biblioteca/exercicio_06.sql
@@ -1,11 +1,15 @@
 /* Quais os livros que foram mais retirados no ano de 2019? */
 
+-- Opção 01
+
 SELECT l.codlivro, l.titulo, COUNT(r.codret) num_retiradas
 FROM livro l,
      retirada r
 WHERE r.dataret BETWEEN '01/01/2019' AND '31/12/2019'
 GROUP BY l.codlivro
 ORDER BY num_retiradas DESC; -- Erro: Falta Junção l.codlivro = r.codlivro
+
+-- Opção 02
 
 SELECT l.codlivro, l.titulo, COUNT(r.codret) num_retiradas
 FROM livro l,
@@ -15,6 +19,8 @@ WHERE l.codlivro = r.codlivro
 GROUP BY l.codlivro
 ORDER BY num_retiradas DESC; -- Correta
 
+-- Opção 03
+
 SELECT l.codlivro, l.titulo, COUNT(r.codret) num_retiradas
 FROM livro l,
      retirada r
@@ -22,6 +28,8 @@ WHERE l.codlivro = r.codlivro
   AND r.dataret BETWEEN '01/01/2019' AND '31/12/2019'
 GROUP BY r.codret
 ORDER BY num_retiradas DESC; -- Erro: Agrupamento errado
+
+-- Opção 04
 
 SELECT l.codlivro, l.titulo, SUM(r.codret) num_retiradas
 FROM livro l,
--- Files/Biblioteca/exercicio_08.sql
+++ Files/Biblioteca/exercicio_08.sql
@@ -16,7 +16,7 @@ da retirada, e o título do livro retirado. */
 
 SELECT r.dataret as data_ret,
        r.horaret as hora_ret,
-       l.titulo as livro
+       l.titulo  as livro
 FROM retirada r,
      livro l
 WHERE r.codlivro = l.codlivro
@@ -25,26 +25,87 @@ WHERE r.codlivro = l.codlivro
 
 /* PARTE 02 */
 
-/* Quais usuários já retiraram o livro que possui o código 1? Mostrar o código, o nome, data
-de nascimento e nome da cidade destes usuários. */
+/* Quais usuários já retiraram o livro que possui o código 1 e também retiraram o livro com código 3?
+   Mostrar o código, o nome, data de nascimento e nome da cidade destes usuários. */
 
 SELECT u.codusu,
        u.nome as nome_usu,
        u.datanasc,
        c.nome as cidade
 FROM usuario u,
+     cidade c
+WHERE u.codcid = c.codcid
+  AND u.codusu IN (
+    SELECT r.codusu
+    FROM retirada r
+    WHERE r.codlivro = 1) -- Livro 1
+  AND u.codusu IN (
+    SELECT r.codusu
+    FROM retirada r
+    WHERE r.codlivro = 4);
+-- Livro 3
+
+-- Não resolve o problema, pois faz uma união e não intersecção
+SELECT u.codusu, u.nome, u.datanasc, c.nome
+FROM usuario u,
      cidade c,
      retirada r
 WHERE u.codcid = c.codcid
   AND u.codusu = r.codusu
-  AND r.codlivro = 1;
+  AND (r.codlivro = 1 OR r.codlivro = 3);
+
+-- Solução equivalente com intersecção explícita
+SELECT u.codusu, u.nome, u.datanasc, c.nome
+FROM usuario u,
+     cidade c,
+     retirada r
+WHERE u.codcid = c.codcid
+  AND u.codusu = r.codusu
+  AND r.codlivro = 1
+INTERSECT
+SELECT u.codusu, u.nome, u.datanasc, c.nome
+FROM usuario u,
+     cidade c,
+     retirada r
+WHERE u.codcid = c.codcid
+  AND u.codusu = r.codusu
+  AND r.codlivro = 3;
+
+/* Criar uma consulta que mostre os livros (código e título) e para cada livro mostre a data,
+hora e o nome do usuário das reservas feitas no ano de 2018 e também das reservas
+feitas no ano de 2019. Na resolução faça uma junção da tabela livro com uma
+subconsulta que traga os dados das reservas de 2018 e outra junção com uma
+subconsulta que traga os mesmos dados para 2019. Ordenar pelo título do livro. */
+
+SELECT l.codlivro,
+       l.titulo,
+       r18.datares,
+       r18.horares,
+       r18.nome as nome_usuario,
+       r19.datares,
+       r19.horares,
+       r19.nome as nome_usuario
+FROM livro l
+         LEFT JOIN
+     (SELECT r.codlivro, r.datares, r.horares, u.nome
+      FROM reserva r,
+           usuario u
+      WHERE r.codusu = u.codusu
+        AND r.datares BETWEEN '01/01/18' AND '31/12/18') r18 ON l.codlivro = r18.codlivro
+         LEFT JOIN
+     (SELECT r.codlivro, r.datares, r.horares, u.nome
+      FROM reserva r,
+           usuario u
+      WHERE r.codusu = u.codusu
+        AND r.datares BETWEEN '01/01/19' AND '31/12/19') r19 ON l.codlivro = r19.codlivro
+ORDER BY l.titulo;
 
 /* Criar uma consulta que mostre um resumo para cada usuário. Exibir o código e o nome
 do usuário, quantas reservas e quantas retiradas ele fez. As contagens devem ser obtidas
 por subconsultas usadas na cláusula SELECT da consulta principal. */
 
 SELECT u.codusu,
-       u.nome as usuario,
-       (SELECT COUNT(r.codres) FROM reserva r WHERE u.codusu = r.codusu) qntd_reservas,
+       u.nome as                                                             usuario,
+       (SELECT COUNT(r.codres) FROM reserva r WHERE u.codusu = r.codusu)     qntd_reservas,
        (SELECT COUNT(r1.codret) FROM retirada r1 WHERE u.codusu = r1.codusu) qntd_retiradas
 FROM usuario u;
